17b127e5fb65ba2702a7ad9427b90fb9d673f09d
kamorenoza <66225199+kamorenoza@users.noreply.github.com>
2026-09-17 00:46:00 +0000

    fix

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.jo7j4q2g7sg"
+    "revision": "0.uv20noet5ps"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
